4dfc9979fe8c4e5ae145b5530f67490570ad069a
Sandra Borboa <sborboaluna@gmail.com>
2025-11-19 12:25:04 -0700

    Feat(MVP): Funciones matemáticas básicas

---
+++ script.js
@@ -0,0 +1,31 @@
+// Inicio
+let a;
+let b;
+
+// Función para sumar
+function add(a, b) {
+    return a + b;
+}
+
+// Función para restar
+function subtrack(a, b) {
+    return a - b;
+}
+
+// Función para multiplicar
+function multiply(a, b) {
+    return a * b;
+}
+
+// Función para dividir
+function divide(a, b) {
+    return a / b;
+}
+
+// Se asigna el return de la función a la variable result
+let result = divide(10, 5);
+
+// Se imprime el resultado en la consola
+console.log(result);
+
+// Fin
